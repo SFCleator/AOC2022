2e130b831ec626041f21d5380798927e9ef1ebfa
Sean F. Cleator <sean@3ug.io>
2022-12-01 16:16:21 +0000

    Second challenge of first day

--- d01/main.py
+++ d01/main.py
@@ -1,14 +1,16 @@
-
+# Get the values
 with open("input.txt") as file:
     vals = file.read().split("\n")
 
+# Find all the totals
 running_val = 0
 summed_vals = []
 for val in vals:
-    if val == '':
+    if val == "":
         summed_vals.append(running_val)
         running_val = 0
     else:
         running_val += int(val)
 
-print(max(summed_vals))
+print("Max: ", max(summed_vals))
+print("Max 3 sum:", sum(sorted(summed_vals)[-3:]))
--- d01/src/main.rs
+++ d01/src/main.rs
@@ -3,10 +3,11 @@ use std::fs;
 fn main() {
     // Read in the file lines to a vector
     let file_path = String::from("input.txt");
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let calories: Vec<&str> = contents.split("\n").collect();
-   
+
+    let mut elf_calories = Vec::new();
+
     // Calculate the max of the blocks
     let mut cur_total = 0;
     let mut max_total = 0;
@@ -23,7 +24,8 @@ fn main() {
         if calorie >= 0 {
             cur_total += calorie;
         } else {
-            println!("{}", cur_total);
+            elf_calories.push(cur_total);
+
             if cur_total > max_total {
                 max_total = cur_total;
             }
@@ -31,5 +33,17 @@ fn main() {
         }
     }
     println!("");
-    println!("Max Value: {}", max_total)
+    println!("Max Value: {}", max_total);
+
+    // Get the largest 3 values
+    elf_calories.sort();
+    let vec_len = elf_calories.len();
+    let largest_3 = elf_calories.get((vec_len - 3)..vec_len).unwrap();
+
+    // Sum the largest three together
+    let mut sum_largest_3 = 0;
+    for elf_cal in largest_3 {
+        sum_largest_3 += elf_cal;
+    }
+    println!("Sum of the largest three: {}", sum_largest_3);
 }
