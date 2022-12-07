68bab48f5938b316722e16e5f6faf935af1495ff
Sean F. Cleator <sean@3ug.io>
2022-12-07 10:48:21 +0000

    Struggling with string mutation

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -15,38 +15,60 @@ fn main() {
             continue
         }
 
-        //let first = &backpack[..length/2].chars();
-        //let second= &backpack[length/2..].chars();
+        let mut in_backpack_common_items = dual_common(backpack, 0, length_half,length_half, length_half*2) ;
 
-        let mut common_items: Vec<String> = Vec::new();
+        value += score_items(&in_backpack_common_items );
 
-        for first_char in backpack[0..length_half].chars() {
-            'inner: for second_char in backpack[length_half..length_half*2].chars() {
-                if common_items.contains(&first_char.to_string()) {
-                    break 'inner;
-                }
-                if first_char == second_char {
-                    common_items.push(first_char.to_string());
-                    break 'inner;
-                }
-            }
-        }
+        //println!("{} {:?} {}",  backpack, in_backpack_common_items, value);
+    }
+    //println!("Result: {}", value);
 
-        // Cheating...
-        let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    for idx in 0..backpacks.len() {
+        let start_idx = idx*3;
 
+        let  mut dual_backpack: Vec<String>= Vec::new();
+        dual_backpack.push_str(&backpacks.get(start_idx).unwrap().to_string());
+        dual_backpack.push_str(&backpacks.get(start_idx+1).unwrap().to_string());
 
-        for cur_char in &common_items {
-            for (idx, cur_alphabet) in alphabet.iter().enumerate(){
-                if cur_alphabet.to_string() == *cur_char {
-                    value += idx + 1;
-                    break
-                }
-            }
-        }
+        let first_string_size: usize = backpacks.get(start_idx).unwrap().len();
 
-        println!("{} {:?} {}",  backpack, common_items, value);
+        let first_two_compare = dual_common(dual_backpack, 0, first_string_size,first_string_size, dual_backpack.len());
+
+        println!("{} {:?}", dual_backpack, first_two_compare);
+
     }
 
-    println!("Result: {}", value);
+}
+
+fn dual_common(items: &str, first_start: usize, first_end: usize, second_start: usize, second_end:usize) -> Vec<String> {
+    let mut common: Vec<String> = Vec::new();
+
+    for first_char in items[first_start..first_end].chars() {
+        'inner: for second_char in items[second_start..second_end].chars() {
+            if common.contains(&first_char.to_string()) {
+                break 'inner;
+            }
+            if first_char == second_char {
+                common.push(first_char.to_string());
+                break 'inner;
+            }
+        }
+    }
+
+    return common;
+}
+
+fn score_items(items: &Vec<String>) -> usize{
+    let mut score =0;
+    let alphabet: Vec<char> = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
+    for cur_char in items{
+        for (idx, cur_alphabet) in alphabet.iter().enumerate(){
+            if cur_alphabet.to_string() == *cur_char {
+                score += idx + 1;
+                break
+            }
+        }
+    }
+
+    return score;
 }
